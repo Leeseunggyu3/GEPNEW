b60e9d2b4469a3b3818856f5fa2dc9638f54bbdb
이승규 <65271132+Leeseunggyu3@users.noreply.github.com>
2024-10-08 11:16:26 +0900

    Edited something related with Arrow
    ArrowController 스크립트 수정
    월드맵 수정
    Arrow 프리팹 수정(Arrow 태그, 레이어 생성)

--- Assets/Scripts/ItemKeeper.cs
+++ Assets/Scripts/ItemKeeper.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class ItemKeeper : MonoBehaviour
 {
     public static int hasKeys = 0; // 열쇠수
-    public static int hasArrows = 0; //화살 소지 수
+    public static int hasArrows = 20; //화살 소지 수
 
 }
